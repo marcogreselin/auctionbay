5a6986f9939484a44145ec7c725cddd83f4a114c
Jeffrey <jeffreyyong10@gmail.com>
2016-01-29 11:29:58 +0000

    Empty files for the header and footer
    TODO: Create the template for the header and footer once we are decided
    about which the structure

---
+++ www/webapp/component/footer.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JeffreyYong
+ * Date: 28/01/2016
+ * Time: 23:11
+ */
+
---
+++ www/webapp/component/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JeffreyYong
+ * Date: 28/01/2016
+ * Time: 23:10
+ */
