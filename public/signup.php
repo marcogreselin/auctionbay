5e3e5e549a5e7b675f42a98090fb5904d5630560
Jeffrey <jeffreyyong10@gmail.com>
2016-01-31 00:23:55 +0000

    Adds address.php and modifies the signup.php
    The data in signup.php is transferred to address.php using the post
    method and the data can be retrieved using the $_POST global variable

--- public/signup.php
+++ public/signup.php
@@ -1,8 +1,13 @@
+
+<?php
+ setcookie("test",45, time()+60*60*24*7);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>AuctionBay - Home</title>
+    <title>AuctionBay</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Loading Bootstrap -->
@@ -14,7 +19,11 @@
     <!-- Loading AB CSS -->
     <link href="css/auctionbay.css" rel="stylesheet">
 
-    
+    <!-- Loading Font Awesome Icons -->
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+
     <link rel="shortcut icon" href="img/favicon.ico">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -36,142 +45,98 @@
 
       <div class="collapse navbar-collapse" id="navbar-collapse-01">
         <ul class="nav navbar-nav">
-    
+
         </ul>
-        
+
       </div><!-- /.navbar-collapse -->
     </nav><!-- /navbar -->
 
 <div class="container">
     <h1>Sign Up</h1>
-    <form class="sign-up-form form-horizontal" role="form">
+
+    <form class="sign-up-form form-horizontal" action="address.php" method="post">
+
 
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="firstname"><h4>First Name:</h4></label>
             <div class="col-sm-8">
-                <input type="text" class="form-control" id="firstname" placeholder="Your first name">
+                <input type="text" class="form-control" name="firstname" value="" id="firstname" placeholder="Your first name">
             </div>
         </div>
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="lastname"><h4>Last Name:</h4></label>
             <div class="col-sm-8">
-                <input type="text" class="form-control" id="lastname" placeholder="Your last name">
+                <input type="text" class="form-control" name="lastname" value="" id="lastname" placeholder="Your last name">
             </div>
         </div>
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="email"><h4>Email:</h4></label>
             <div class="col-sm-8">
-                <input type="email" class="form-control" id="email" placeholder="Enter email">
+                <input type="email" class="form-control" name="email" value="" id="email" placeholder="Enter email">
             </div>
         </div>
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="pwd"><h4>Password:</h4></label>
             <div class="col-sm-8">
-                <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                <input type="password" class="form-control" name="password" value="" id="pwd" placeholder="Enter password">
             </div>
         </div>
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="pwdagain"><h4>Password again:</h4></label>
             <div class="col-sm-8">
-                <input type="password" class="form-control" id="pwdagain" placeholder="Enter password again">
+                <input type="password" class="form-control" name="passwordagain" value="" id="pwdagain" placeholder="Enter password again">
             </div>
         </div>
 
-
-        <form class="role-check-form">
-            <div class="container text-center">
-                <div class="form-group container-survey-radio">
-                    <div class="row">
-                        <div class="col-xs-2 col-xs-offset-4">
-                            <label class="radio-inline control-label">
-                                <div class="form-group container-survey-yesno">
-                                    <input class="role-checkbox"
-                                           type="radio"
-                                           id="role-checkbox-buyer"
-                                           name="role-check"
-                                           value="yes"/>
-                                    <label class="role-check-label" for="role-checkbox-buyer">
-                                        Buyer
-                                    </label>
-                                </div>
-                            </label>
-                        </div>
+        <div class="container text-center">
+            <div class="form-group container-survey-radio">
+                <div class="row">
+                    <div class="col-xs-2 col-xs-offset-4">
+                        <label class="radio-inline control-label">
+                            <div class="form-group container-survey-yesno">
+                                <input class="role-checkbox"
+                                       type="radio"
+                                       id="role-checkbox-buyer"
+                                       name="role-check"
+                                       value="buyer"/>
+                                <label class="role-check-label" for="role-checkbox-buyer">
+                                    Buyer
+                                </label>
+                            </div>
+                        </label>
+                    </div>
 
 
-                        <div class="col-xs-6">
-                            <label class="radio-inline control-label">
-                                <div class="form-group container-survey-yesno">
-                                    <input class="role-checkbox"
-                                           type="radio"
-                                           id="role-checkbox-seller"
-                                           name="role-check"
-                                           value="no"/>
-                                    <label class="role-check-label" for="role-checkbox-seller">
-                                        Seller
-                                    </label>
-                                </div>
-                            </label>
-                        </div>
+                    <div class="col-xs-6">
+                        <label class="radio-inline control-label">
+                            <div class="form-group container-survey-yesno">
+                                <input class="role-checkbox"
+                                       type="radio"
+                                       id="role-checkbox-seller"
+                                       name="role-check"
+                                       value="seller"/>
+                                <label class="role-check-label" for="role-checkbox-seller">
+                                    Seller
+                                </label>
+                            </div>
+                        </label>
                     </div>
                 </div>
             </div>
-        </form>
-
-        <div class="form-group form-group-hg">
-            <div class="col-sm-8 col-sm-offset-4 text-center">
-                <button class="btn btn-hg btn-primary btn-block">
-                    Sign Up!
-                </button>
-            </div>
         </div>
 
 
-
-        <!--<div class="form-group">-->
-        <!--<div class="col-sm-offset-2 col-sm-10">-->
-        <!--<div class="checkbox">-->
-        <!--<label><input type="checkbox"> Remember me</label>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="form-group">-->
-        <!--<div class="col-sm-offset-2 col-sm-10">-->
-        <!--<button type="submit" class="btn btn-default">Submit</button>-->
-        <!--</div>-->
-        <!--</div>-->
-
-
-        <!--<div class="form-group form-group-hg">-->
-        <!--<label class="col-sm-2">-->
-        <!--<h4>First Name:</h4>-->
-        <!--</label>-->
-        <!--<div class="col-sm-10">-->
-        <!--<input class="form-control"-->
-        <!--type="text"-->
-        <!--id="firstname"-->
-        <!--placeholder="First Name"/>-->
-        <!--</div>-->
-        <!--</div>-->
-
-        <!--<div class="form-group form-group-hg">-->
-        <!--<label class="col-sm-2">-->
-        <!--<h4>Testing:</h4>-->
-        <!--</label>-->
-        <!--<div class="col-sm-10">-->
-        <!--<input class="form-control"-->
-        <!--type="text"-->
-        <!--id="lastname"-->
-        <!--placeholder="Testing"/>-->
-        <!--</div>-->
-        <!--</div>-->
-
-
-        <!--<button class="btn-hg" text="Sign Up"/>-->
+        <!-- Submitting form will post the data to address.php to be used -->
+        <div class="form-group form-group-hg">
+            <div class="col-sm-8 col-sm-offset-4 text-center">
+                <input class="btn btn-hg btn-primary btn-block" type="submit" name="submit" value="Continue to enter you address">
+            </div>
+        </div>
     </form>
 </div>
 <!-- /.container -->
