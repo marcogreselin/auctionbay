5d6dc3f2959ec78cb1669ae401e82363a622722b
nterreri <niccolo.terreri@gmail.com>
2016-02-03 09:15:12 +0000

    initial commit

---
+++ includes/validation_functions.php
@@ -0,0 +1,57 @@
+<?php
+/*form input validation functions,
+  see http://www.lynda.com/MySQL-tutorials/Welcome/119003/136910-4.html*/
+
+$errors = array();
+
+function fieldname_as_text($fieldname) {
+  $fieldname = str_replace("_", " ", $fieldname);
+  $fieldname = ucfirst($fieldname);
+  return $fieldname;
+}
+
+// * presence
+// use trim() so empty spaces don't count
+// use === to avoid false positives
+// empty() would consider "0" to be empty
+function has_presence($value) {
+	return isset($value) && $value !== "";
+}
+
+function validate_presences($required_fields) {
+  global $errors;
+  foreach($required_fields as $field) {
+    $value = trim($_POST[$field]);
+  	if (!has_presence($value)) {
+  		$errors[$field] = fieldname_as_text($field) . " can't be blank";
+  	}
+  }
+}
+
+// * string length
+// max length
+function has_max_length($value, $max) {
+	return strlen($value) <= $max;
+}
+
+function validate_max_lengths($fields_with_max_lengths) {
+	global $errors;
+	// Expects an assoc. array
+	foreach($fields_with_max_lengths as $field => $max) {
+		$value = trim($_POST[$field]);
+	  if (!has_max_length($value, $max)) {
+	    $errors[$field] = fieldname_as_text($field) . " is too long";
+	  }
+	}
+}
+
+function match($first_field, $second_field) {
+  global $errors;
+
+  //strcmp returns 0 where the strings match, see http://php.net/manual/en/function.strcmp.php
+  if(strcmp($first_field, $second_field)){
+    $errors[$first_field] = fieldname_as_text($first_field) . "s do not match";
+  }//else do nothing
+}
+
+?>
---
+++ public/signup.php
@@ -0,0 +1,152 @@
+<?php
+//  Cookies
+ setcookie("test",45, time()+60*60*24*7);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>AuctionBay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+
+    <!-- Loading AB CSS -->
+    <link href="css/auctionbay.css" rel="stylesheet">
+
+    <!-- Loading Font Awesome Icons -->
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-default navbar-lg" role="navigation">
+      <div class="navbar-header">
+        <a class="navbar-brand"><div class="logo-small"></div>AuctionBay</a>
+      </div>
+            <p class="navbar-text navbar-right">Return to the <a class="navbar-link" href="index.php">Home Page</a></p>
+
+      <div class="collapse navbar-collapse" id="navbar-collapse-01">
+        <ul class="nav navbar-nav">
+
+        </ul>
+
+      </div><!-- /.navbar-collapse -->
+    </nav><!-- /navbar -->
+
+<div class="container">
+    <h1>Sign Up</h1>
+
+    <form class="sign-up-form form-horizontal" action="address.php" method="post">
+
+
+
+        <div class="form-group form-group-hg">
+            <label class="control-label col-sm-4" for="firstname"><h4>First Name:</h4></label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="firstname" value="" id="firstname" placeholder="Your first name">
+            </div>
+        </div>
+
+        <div class="form-group form-group-hg">
+            <label class="control-label col-sm-4" for="lastname"><h4>Last Name:</h4></label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="lastname" value="" id="lastname" placeholder="Your last name">
+            </div>
+        </div>
+
+        <div class="form-group form-group-hg">
+            <label class="control-label col-sm-4" for="email"><h4>Email:</h4></label>
+            <div class="col-sm-8">
+                <input type="email" class="form-control" name="email" value="" id="email" placeholder="Enter email">
+            </div>
+        </div>
+
+        <div class="form-group form-group-hg">
+            <label class="control-label col-sm-4" for="pwd"><h4>Password:</h4></label>
+            <div class="col-sm-8">
+                <input type="password" class="form-control" name="password" value="" id="pwd" placeholder="Enter password">
+            </div>
+        </div>
+
+        <div class="form-group form-group-hg">
+            <label class="control-label col-sm-4" for="pwdagain"><h4>Password again:</h4></label>
+            <div class="col-sm-8">
+                <input type="password" class="form-control" name="passwordagain" value="" id="pwdagain" placeholder="Enter password again">
+            </div>
+        </div>
+
+        <div class="container text-center">
+            <div class="form-group container-survey-radio">
+                <div class="row">
+                    <div class="col-xs-2 col-xs-offset-4">
+                        <label class="radio-inline control-label">
+                            <div class="form-group container-survey-yesno">
+                                <input class="role-checkbox"
+                                       type="radio"
+                                       id="role-checkbox-buyer"
+                                       name="role-check"
+                                       value="buyer"/>
+                                <label class="role-check-label" for="role-checkbox-buyer">
+                                    Buyer
+                                </label>
+                            </div>
+                        </label>
+                    </div>
+
+
+                    <div class="col-xs-6">
+                        <label class="radio-inline control-label">
+                            <div class="form-group container-survey-yesno">
+                                <input class="role-checkbox"
+                                       type="radio"
+                                       id="role-checkbox-seller"
+                                       name="role-check"
+                                       value="seller"/>
+                                <label class="role-check-label" for="role-checkbox-seller">
+                                    Seller
+                                </label>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Submitting form will post the data to address.php to be used -->
+        <div class="form-group form-group-hg">
+            <div class="col-sm-8 col-sm-offset-4 text-center">
+                <input class="btn btn-hg btn-primary btn-block" type="submit" name="submit" value="Continue to enter you address">
+            </div>
+        </div>
+    </form>
+</div>
+<!-- /.container -->
+
+
+<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
+<script src="../dist/js/vendor/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../dist/js/vendor/video.js"></script>
+<script src="../dist/js/flat-ui.min.js"></script>
+
+</body>
+</html>
