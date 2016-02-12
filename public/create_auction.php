c418d4e5ec2d1797eb0a3c51b150afb2352623cc
Marco Greselin <marcogreselin@me.com>
2016-02-12 12:07:31 +0000

    Add Auction

---
+++ public/create_auction.php
@@ -0,0 +1,77 @@
+<?php
+  include("../includes/layouts/header_buyer.php");
+?>
+
+<div class="container">
+	<h1>Create an Auction</h1>
+
+	<p><input type="text" class="form-control input-hg auction-form" placeholder="Enter the Title" /></p>
+	<p><textarea class="form-control" rows="15" id="comment" placeholder="Enter the Body"></textarea></p>
+	<div class="row">
+		<div class="col-md-6">
+			<p>Select an image:</p>
+			<div class="form-group">
+			  <div class="fileinput fileinput-new" data-provides="fileinput">
+			    <div class="input-group">
+			      <div class="form-control uneditable-input" data-trigger="fileinput">
+			        <span class="fui-clip fileinput-exists"></span>
+			        <span class="fileinput-filename"></span>
+			      </div>
+			      <span class="input-group-btn btn-file">					    	
+			        <span class="btn btn-default fileinput-new" data-role="select-file">Select file</span>
+			        <span class="btn btn-default fileinput-exists" data-role="change"><span class="fui-gear"></span>  Change</span>
+			        <input type="file" name="...">
+			        <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput"><span class="fui-trash"></span>  Remove</a>					    	
+			      </span>					    
+			    </div>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<p>When should your auction end?</p>
+			<div class="form-group">
+			  <div class="input-group">
+			    <span class="input-group-btn">
+			      <button class="btn" type="button"><span class="fui-calendar"></span></button>
+			    </span>
+			    <input type="text" class="form-control" value="14 March, 2015" id="datepicker-01" />
+			  </div>
+			</div>
+		</div>
+
+		
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+		<p>Enter your <b>starting</b> price:</p>
+		<div class="form-group">
+		  <div class="input-group">
+		    <span class="input-group-addon">&pound;</span>
+		    <input type="text" class="form-control" placeholder="" />
+		  </div>  
+		</div>
+		</div>
+
+		<div class="col-md-6">
+		<p>Enter your <b>reserve</b> price:</p>
+		<div class="form-group">
+		  <div class="input-group">
+		    <span class="input-group-addon">&pound;</span>
+		    <input type="text" class="form-control" placeholder="" />
+		  </div>  
+		</div>
+		</div>
+	</div>
+	<button onclick="location.href='index.php'" class="btn btn-hg btn-primary create-button">
+	  Create Auction
+	</button>
+	<span class="countdown"></span>
+</div>
+
+
+
+
+
+<?php
+  include("../includes/layouts/footer.php");
+?>
--- public/search.php
+++ public/search.php
@@ -1,79 +1,15 @@
 <?php
-  setcookie("test",45,time()+60*60*24*7);
+  include("../includes/layouts/header_buyer.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>My Account</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="container text-center">
+  <div class="login-headline">Search!</div>
+  <p><input type="text" class="form-control input-hg search-box" placeholder="What do you want to search?" /></p>
+  <p><button onclick="location.href='buyer_account.php'" class="btn btn-hg btn-primary">
+      Search
+  </button></p>
+</div>
 
-    <!-- Loading Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Loading Flat UI -->
-    <link href="css/flat-ui.css" rel="stylesheet">
-
-    <!-- Loading AB CSS -->
-    <link href="css/auctionbay.css" rel="stylesheet">
-
-    
-    <link rel="shortcut icon" href="img/favicon.ico">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="js/vendor/html5shiv.js"></script>
-      <script src="js/vendor/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-default navbar-lg" role="navigation">
-      <div class="navbar-header">
-        <a class="navbar-brand"><div class="logo-small"></div>AuctionBay</a>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
-          <span class="sr-only">Toggle navigation</span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse" id="navbar-collapse-01">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#fakelink">My Account</a></li>
-          <li><a href="">Search</a></li>
-        </ul>
-        <p class="navbar-text navbar-right">
-          <a class="navbar-link" href="index.php">Logout</a>
-        </p>
-        <form class="navbar-form navbar-right" action="searchpage.php" role="search">
-          <div class="form-group">
-            <div class="input-group">
-              <input class="form-control" id="navbarInput-01" type="search" placeholder="Search">
-              <span class="input-group-btn">
-                <button type="submit" class="btn"><span class="fui-search"></span></button>
-              </span>
-            </div>
-          </div>
-        </form>
-      </div><!-- /.navbar-collapse -->
-    </nav><!-- /navbar -->
-
-
-    <div class="container text-center">
-      <div class="login-headline">Search!</div>
-      <p><input type="text" class="form-control input-hg search-box" placeholder="What do you want to search?" /></p>
-      <p><button onclick="location.href='buyer_account.php'" class="btn btn-hg btn-primary">
-          Search
-      </button></p>
-    </div>
-
-    <!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
-    <script src="js/vendor/jquery.min.js"></script>
-    <script src="js/vendor/video.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/flat-ui-pro.js"></script>
-    <!-- This is for the smooth scrolling. See http://stackoverflow.com/questions/7717527/jquery-smooth-scrolling-when-clicking-an-anchor-link -->
-
-
-  </body>
-</html>
+<?php
+  include("../includes/layouts/footer.php");
+?>
