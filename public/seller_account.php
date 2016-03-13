6df73fef903ba63e5908df8d84e6fc57be707dee
Marco Greselin <marcogreselin@me.com>
2016-03-13 17:38:43 +0000

    minor changes to account page

--- public/seller_account.php
+++ public/seller_account.php
@@ -99,7 +99,7 @@
 
        ?>
 <!--Maybe add an "edit" button here, would require another query to user table-->
-   <a name="address"><h3>My Details</h3></a> <?php echo "<p>Average Feedback: <a href='feedback.php?for_user_id={$userId}'>"  . $average_stars_user['stars'] . "stars</a>.</p>";?>
+   <a name="address"><h3>My Details</h3></a> <?php echo "<p>Average Feedback: <a href='feedback.php?for_user_id={$userId}'>"  . $average_stars_user['stars'] . " stars</a>.</p>";?>
    <p><b>My Address:</b><br>
    <?php echo $_SESSION['firstName'] . " " . "<br>"; ?>
    <?php echo $_SESSION['street'] . "<br>"; ?>
