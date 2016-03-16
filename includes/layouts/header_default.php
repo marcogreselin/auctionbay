cb9965f80c8d01146c7730320535f5a23caa0823
Jeffrey <jeffreyyong10@gmail.com>
2016-03-16 18:31:09 +0000

    Code cleaning for address.php and signup.php
    header_defautl is the general header for both signup.php and address.php

---
+++ includes/layouts/header_default.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>AuctionBay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="css/flat-ui.css" rel="stylesheet">
+
+    <!-- Loading AB CSS -->
+    <link href="css/auctionbay.css" rel="stylesheet">
+
+    <!-- Loading Font Awesome Icons -->
+    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/vendor/html5shiv.js"></script>
+    <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-default navbar-lg" role="navigation">
+    <div class="navbar-header">
+        <a class="navbar-brand"><div class="logo-small"></div>AuctionBay</a>
+    </div>
+    <p class="navbar-text navbar-right">Return to the <a class="navbar-link" href="index.php">Home Page</a></p>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse-01">
+        <ul class="nav navbar-nav">
+
+        </ul>
+
+    </div><!-- /.navbar-collapse -->
+</nav><!-- /navbar -->
--- public/signup.php
+++ public/signup.php
@@ -15,57 +15,8 @@ $firstname  = repeat_input("firstname");
 $lastname   = repeat_input("lastname");
 $email      = repeat_input("email");
 
-
+require_once("../includes/layouts/header_default.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
-<head>
-    <meta charset="utf-8">
-    <title>AuctionBay</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Loading Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Loading Flat UI -->
-    <link href="css/flat-ui.css" rel="stylesheet">
-
-    <!-- Loading AB CSS -->
-    <link href="css/auctionbay.css" rel="stylesheet">
-
-    <!-- Loading Font Awesome Icons -->
-    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
-
-    <link rel="shortcut icon" href="img/favicon.ico">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="js/vendor/html5shiv.js"></script>
-      <script src="js/vendor/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-
-<body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-default navbar-lg" role="navigation">
-      <div class="navbar-header">
-        <a class="navbar-brand"><div class="logo-small"></div>AuctionBay</a>
-      </div>
-            <p class="navbar-text navbar-right">Return to the <a class="navbar-link" href="index.php">Home Page</a></p>
-
-      <div class="collapse navbar-collapse" id="navbar-collapse-01">
-        <ul class="nav navbar-nav">
-
-        </ul>
-
-      </div><!-- /.navbar-collapse -->
-    </nav><!-- /navbar -->
-
 
     <div class="row">
         <div class="col-sm-4 header-signup">
@@ -95,11 +46,7 @@ $email      = repeat_input("email");
     </div>
 
 
-
-
     <form class="sign-up-form form-horizontal" action="address.php" method="post">
-
-
 
         <div class="form-group form-group-hg">
             <label class="control-label col-sm-4" for="firstname"><h4>First Name:</h4></label>
@@ -201,14 +148,7 @@ $email      = repeat_input("email");
 <!-- /.container -->
 
 
-<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
-<script src="js/vendor/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/vendor/video.js"></script>
-<script src="js/flat-ui-pro.js"></script>
-
-</body>
-</html>
 <?php
+require_once('../includes/layouts/footer.php');
 //clear session when done rendering page
 clear_session(); ?>
